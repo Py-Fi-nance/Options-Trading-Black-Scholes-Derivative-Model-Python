6564e694916ecf369fa888cfa952e35a481b3037
usag1r <umutsagir@gmail.com>
2023-08-21 16:48:03 +0100

    AMD Example

--- Black_Scholes_model.py
+++ Black_Scholes_model.py
@@ -2,11 +2,11 @@ from openbb_terminal.sdk import openbb
 from math import log, sqrt, exp
 from scipy.stats import norm
 
-S = 425.90
-X = 430
-T = 23/252
+S = 106.25
+X = 110
+T = 18/252
 r = 0.04
-sigma = 79/100
+sigma = 70/100
 d1 = (log(S/X)+(r+sigma**2/2.)*T)/(sigma*sqrt(T))
 d2 = (d1-sigma*sqrt(T))
 
@@ -17,8 +17,8 @@ Put_price = X*exp(-r*T)*norm.cdf(-d2)-(S*norm.cdf(-d1))
 print(('Call: {}, Put: {}'.format(Call_price, Put_price)))
 
 
-df=openbb.stocks.options.chains("NVDA", expiration="2023-10-20")
-df=df.loc[df['strike']==430]
+df=openbb.stocks.options.chains("AMD", expiration="2023-10-20")
+df=df.loc[df['strike']==110]
 print(df)
 
 
